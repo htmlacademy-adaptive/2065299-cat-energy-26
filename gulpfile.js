28f29d7527370336e629d518cb4e5085a700fa10
Mamonova Olga <shamrilo91@mail.ru>
2022-09-15 17:47:37 +0300

    правит сборку

--- gulpfile.js
+++ gulpfile.js
@@ -92,6 +92,7 @@ const copy = (done) => {
   gulp.src([
   'source/fonts/*.{woff2,woff}',
   'source/*.ico',
+  'source/*.webmanifest'
   ], {
   base: 'source'
   })
@@ -130,7 +131,7 @@ const reload = (done) => {
 
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
-  gulp.watch('source/js/script.js', gulp.series(scripts));
+  gulp.watch('source/js/script.js', gulp.series(scripts, reload));
   gulp.watch('source/*.html', gulp.series(html, reload));;
 }
 
